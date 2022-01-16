01f79a054a39c15f409669a2f7550741e769b6b2
Giutear <gsawczak@gmx.de>
2022-01-16 01:56:38 +0100

    Models can now be stored and loaded correctly

--- serialize.py
+++ serialize.py
@@ -15,6 +15,18 @@ def serialize_neurons(n:neurons.Neuron) -> dict:
         print(f"Unkown neuron type: {type(n)}.")
     return neuron_data
 
+def deserialize_neurons(neuron_data:dict) -> neurons.Neuron:
+    n = neurons.Neuron()
+    if neuron_data["type"] == "RBF":
+        n:neurons.RBF = neurons.RBF()
+        n._c = neuron_data["c"]
+        n._sigma = neuron_data["sigma"]
+        n.num_neurons = n._c.shape[0]
+    else:
+        print(f"Unkown neuron type: {neuron_data['type']}.")
+    return n
+
+
 
 def serialize_layer(l:layer.Layer) -> dict:
     layer_data = {}
@@ -35,9 +47,34 @@ def serialize_layer(l:layer.Layer) -> dict:
         print(f"Unkown layer type: {type(l)}.")
     return layer_data
 
+def deserialize_layer(layer_data:dict) -> layer.Layer:
+    l = layer.Layer()
+    if layer_data["type"] == "Dense":
+        l:layer.DenseLayer = layer.DenseLayer(1, 1, neurons.Neuron())
+        l.neurons = deserialize_neurons(layer_data["neurons"])
+    elif layer_data["type"] == "Conv":
+        l:layer.ConvolutionalLayer = layer.ConvolutionalLayer((1,1,1,1), 1, 1, neurons.RBF())
+        l.neurons = deserialize_neurons(layer_data["neurons"])
+        l.kernel_dimension = layer_data["kernel_dimension"]
+        l.num_horizontal = layer_data["num_horizontal"]
+        l.num_vertical = layer_data["num_vertical"]
+        l.original_shape = layer_data["original_shape"]
+        l.stride = layer_data["stride"]
+    else:
+        print(f"Unkown layer type: {layer_data['type']}.")
+    return l
+
 def serialize_model(model:Model, path:Path):
     model_data = []
     for l in model.layers:
         model_data.append(serialize_layer(l))
     with open(path, "wb") as f:
         pickle.dump(model_data, f)
+
+def deserialize_model(path:Path) -> Model:
+    with open(path, "rb") as f:
+        model_data = pickle.load(f)
+    layers = []
+    for l in model_data:
+        layers.append(deserialize_layer(l))
+    return Model(layers)
